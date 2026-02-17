1cac81abc2267a2fdebc89fd8b406c7edfc6d2f4
Rizwan-jabbar <rizwanhh99@gmail.com>
2026-02-17 21:01:29 +0500

    new files

--- back-end/server.js
+++ back-end/server.js
@@ -13,10 +13,10 @@ const app = express();
 
 
 cloudinary.config({
-  cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-  API_key : process.env.CLOUDINARY_API_KEY,
-  API_secret : process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 // Fix for __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
