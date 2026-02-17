185d8a1714a65ec581f52942413589ab5e7fa51d
Rizwan-jabbar <rizwanhh99@gmail.com>
2026-02-17 12:40:21 +0500

    fix cors  error

--- front-end/src/components/login/login.jsx
+++ front-end/src/components/login/login.jsx
@@ -20,7 +20,7 @@ function Login() {
     };
 
     try {
-      const response = await fetch("http://todo4students.com/api/login", {
+      const response = await fetch("https://todo4students.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
