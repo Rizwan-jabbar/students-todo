3e8f17760211842bc37558c24da7017e62fd597b
Rizwan-jabbar <rizwanhh99@gmail.com>
2026-02-17 14:14:49 +0500

    new files

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -6,14 +6,18 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-export async function dbConnect() {
+const connectDB = async () => {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONGO_URL, {
+      // optional settings can go here
+      // useNewUrlParser: true, useUnifiedTopology: true
     }).then(m => m);
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
-}
+};
+
+export default connectDB;
